595145fa9a4290bfe59a13ce61c39b607cd8ed85
Mika Lehtimäki <Mika.Lehtimaki@icloud.com>
2024-09-06 15:00:40 +0300

    Update LocalT_ESG_RAG_1.25.py

--- LocalT_ESG_RAG_1.25.py
+++ LocalT_ESG_RAG_1.25.py
@@ -32,19 +32,6 @@ from langchain.prompts import PromptTemplate
 import joblib
 import nest_asyncio  # noqa: E402
 nest_asyncio.apply()
-import nltk
-import os
-
-# Specify a custom directory for nltk data (e.g., within your home directory)
-nltk_data_dir = os.path.expanduser('~/nltk_data')
-if not os.path.exists(nltk_data_dir):
-    os.makedirs(nltk_data_dir)
-
-# Set NLTK to use this directory
-nltk.data.path.append(nltk_data_dir)
-
-# Download the required NLTK resources
-nltk.download('punkt', download_dir=nltk_data_dir)
 
 load_dotenv()
 load_dotenv(find_dotenv())
